2e19c5c9682192925265f60a2be9ee643138b647
Aditya Kumar <adityaverma.dev@gmail.com>
2022-10-18 19:16:32 +0530

    details updated

--- user_band_groups/create.js
+++ user_band_groups/create.js
@@ -3,21 +3,41 @@
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const uuid = require('uuid');
-const {getUserDetail} = require('../utils/utils');
+const  { getUserByEmail } = require('../users/get-userby-email');
 
 module.exports = async (event, callback) => {
     const data = JSON.parse(event.body);
   
+    const email = event?.headers?.['x-auth'];
+
+    if(!email) {
+      let error = {
+        errorType : "Validation",
+        errorMessage: "User email is missing!!!",
+        trace : [ "User details are missing!!!" ]
+      }
+      callback(error);
+    }
+
     if(!data?.name || !data?.latitude || data?.latitude || data?.description) {
       let error = {
         errorType : "Validation",
-        errorMessage: "Band name, latitude, latitude or description is missing!!!",
+        errorMessage: "Band name, latitude, latitude, description or admin details are missing is missing!!!",
         trace : [ "Band details are missing!!!" ]
       }
       callback(error);
     }
   
-    let da = await getUserDetail(email);
+    let userDetail = await getUserByEmail(email);
+    data?.admin = [];
+    data?.participants = [];
+    if(userDetail?.id && userDetail?.email) {
+      data?.admin?.push({
+        id: userDetail?.id,
+        name: userDetail?.name,
+        email: userDetail?.email
+      });
+    }
 
     data.id = uuid.v1();
     data.createdAt = new Date().getTime();
@@ -25,7 +45,8 @@ module.exports = async (event, callback) => {
   
     const params = {
       TableName: 'bandgroup',
-      Item: { data: data?.name, latitude: data?.latitude, longitude: data?.longitude, data: data?.description, createdAt: data?.createdAt, updatedAt: data.updatedAt }
+      Item: { data: data?.name, latitude: data?.latitude, longitude: data?.longitude, data: data?.description, 
+        admin: data?.admin, participants: data?.participants, createdAt: data?.createdAt, updatedAt: data.updatedAt }
     };
   
     return dynamoDb.put(params, (error, data) => {
--- users/get-userby-email.js
+++ users/get-userby-email.js
@@ -6,57 +6,33 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const getUserByEmail = async(email) => {
     let userDetail = null;
     try {
-        if(!email) {
-            let error = {
-                errorType : "Validation",
-                errorMessage: "Band name, latitude, latitude or description is missing!!!",
-                trace : [ "Band details are missing!!!" ]
-            }
-            return userDetail;
-          }
-        
-        //   const params2 = {
-        //     TableName: 'users',
-        //     Key: {
-        //       pk: {
-        //         "S": `${email}`
-        //       }
-        //     },
-        //     KeyConditionExpression: '#email = :email',
-        //     ExpressionAttributeNames: {
-        //       "#email": "email"
-        //     },
-        //     ExpressionAttributeValues: {
-        //       ':email' : `${email}`
-        //     }
-        //   };
-
-        const params2 = {
-            TableName: 'users',
-            KeyConditionExpression: '#email = :email',
-            ExpressionAttributeNames: {
-              "#email": "email"
-            },
-            ExpressionAttributeValues: {
-              ':email' : `${email}`
-            },
-          };
-
-        //   return dynamoDb.query(params2, (error, data) => {
-        //     if (error) {
-        //       callback(error);
-        //     }
-        //     console.log(`data item 2: ${data?.Items}`);
-        //     callback(error, data?.Items);
-        //   });
-        userDetail =  await dynamoDb.query(params2).promise();
-        if(userDetail?.Items?.length>0) {
-            userDetail = userDetail?.Items[0];
+      if(!email) {
+        let error = {
+          errorType : "Validation",
+          errorMessage: "Band name, latitude, latitude or description is missing!!!",
+          trace : [ "Band details are missing!!!" ]
         }
-    } catch(ex) {
-        console.log(`exception is: ${userDetail}`);
-    } finally {
+        console.log('email not found!!!');
         return userDetail;
+      }
+      const params2 = {
+        TableName: 'users',
+        KeyConditionExpression: '#email = :email',
+        ExpressionAttributeNames: {
+          "#email": "email"
+        },
+        ExpressionAttributeValues: {
+          ':email' : `${email}`
+        },
+      };
+      userDetail =  await dynamoDb.query(params2).promise();
+      if(userDetail?.Items?.length>0) {
+        userDetail = userDetail?.Items[0];
+      }
+    } catch(ex) {
+      console.log(`exception is: ${userDetail}`);
+    } finally {
+      return userDetail;
     }
 }
 
