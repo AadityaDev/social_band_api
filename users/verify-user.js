0554f68959fdc1d31defd15bd7e9f4beea851a56
Aditya Kumar <adityaverma.dev@gmail.com>
2022-10-18 21:10:55 +0530

    signin added

--- users/verify-user.js
+++ users/verify-user.js
@@ -2,17 +2,18 @@
 
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const  { getUserByEmail } = require('../users/get-userby-email');
+const bcrypt = require("bcryptjs");
 
 module.exports = async (event, callback) => {
+  let userDetail;
   try{  
     const params = {
       TableName: 'users',
     };
-
     console.log(`token 11`);
-    const email = event?.headers?.['x-auth'];
+    // const email = event?.headers?.['x-auth'];
     const data = JSON.parse(event?.body);
-
     if(!data?.email || !data?.password) {
       let error = {
         errorType : "Validation",
@@ -21,16 +22,14 @@ module.exports = async (event, callback) => {
       }
       callback(error);
     }
-
-    let userDetail = await getUserByEmail(email);
-
-    console.log(`token value is: ${JSON.stringify(userDetail)} and ${email}`);
-
+    userDetail = await getUserByEmail(data?.email);
+    console.log(`token value is: ${JSON.stringify(userDetail)} and ${data?.email}`);
     var passwordIsValid = bcrypt.compareSync(
       data?.password,
       userDetail?.password
     );
 
+    console.log(`data is: ${passwordIsValid}`)
     if (!passwordIsValid) {
       console.log(`password is invalid!!!`);
       let error = {
@@ -40,10 +39,10 @@ module.exports = async (event, callback) => {
       }
       callback(error);
     }
-  }
-  catch(ex) {
+  } catch(ex) {
     console.log(`exceptio in verify is: ${ex}`);
+    callback(error);
   } finally {
-    return callback(null, userDetail);
+    callback(null, userDetail);
   }
 };
