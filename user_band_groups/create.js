3c3cc7a3ae70583c83f065128397472cb7d73175
Aditya Kumar <adityaverma.dev@gmail.com>
2022-10-18 22:03:33 +0530

    details added

--- user_band_groups/create.js
+++ user_band_groups/create.js
@@ -6,39 +6,44 @@ const uuid = require('uuid');
 const  { getUserByEmail } = require('../users/get-userby-email');
 
 module.exports = async (event, callback) => {
-    // const data = JSON.parse(event.body);
+  let admins = [];
+    let data = {...JSON.parse(event.body), admins };
   
-    // const email = event?.headers?.['x-auth'];
+    const email = event?.headers?.['x-auth'];
 
-    // if(!email) {
-    //   let error = {
-    //     errorType : "Validation",
-    //     errorMessage: "User email is missing!!!",
-    //     trace : [ "User details are missing!!!" ]
-    //   }
-    //   callback(error);
-    // }
+    if(!email) {
+      let error = {
+        errorType : "Validation",
+        errorMessage: "User email is missing!!!",
+        trace : [ "User details are missing!!!" ]
+      }
+      callback(error);
+    }
 
-    // if(!data?.name || !data?.latitude || !data?.latitude || !data?.description) {
-    //   let error = {
-    //     errorType : "Validation",
-    //     errorMessage: "Band name, latitude, latitude, description or admin details are missing is missing!!!",
-    //     trace : [ "Band details are missing!!!" ]
-    //   }
-    //   callback(error);
-    // }
+    if(!data?.name || !data?.latitude || !data?.latitude || !data?.description) {
+      let error = {
+        errorType : "Validation",
+        errorMessage: "Band name, latitude, latitude, description or admin details are missing is missing!!!",
+        trace : [ "Band details are missing!!!" ]
+      }
+      callback(error);
+    }
   
-    // let userDetail = await getUserByEmail(email);
+    let userDetail = await getUserByEmail(email);
+    console.log(`user details: ${JSON.stringify(userDetail)}`);
     // data?.admin = [];
     // data?.participants = [];
-    // if(!userDetail?.id && !userDetail?.email) {
-    //   data?.admin?.push({
-    //     id: userDetail?.id,
-    //     name: userDetail?.name,
-    //     email: userDetail?.email,
-    //     approved: true
-    //   });
-    // }
+    console.log(`${userDetail?.id} and ${userDetail?.email} => ${!userDetail?.id && !userDetail?.email}`);
+    if(userDetail?.id && userDetail?.email) {
+      console.log('reached here');
+      data.admin?.push({
+        id: userDetail?.id,
+        name: userDetail?.name,
+        email: userDetail?.email,
+        approved: true
+      });
+    }
+    console.log(`user details: ${JSON.stringify(data)}`);
 
     // data.id = uuid.v1();
     // data.createdAt = new Date().getTime();
